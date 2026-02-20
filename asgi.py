c6fcc9f021e6432b14bf7cc99687da8722a19ccf
Meche-57 <93935558+Meche-57@users.noreply.github.com>
2026-02-20 13:01:16 +0000

    -Bug Fix file name was incorrect

--- asgi.py
+++ asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for finance_appBackend project.
+ASGI config for finance_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance_appBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance_backend.settings')
 
 application = get_asgi_application()
--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for finance_appBackend project.
+URL configuration for finance_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
